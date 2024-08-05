db20cc1fe61626718b7b57bcf32a4f431fe7009a
Jacob Atterson <atterjac000@warren.k12.in.us>
2024-08-05 13:32:12 -0400

    Math

---
+++ src/Prim.java
@@ -0,0 +1,12 @@
+public class Prim{
+
+    public static void main(String[] args) {
+        byte myByte = 9;
+        short myShort = 8;
+        int myInt = 7;
+        int myLong = 24327870832378780243L;
+
+
+    }
+
+}
